aab18d898167118e173c9c37090ae2615dc688a1
shivam <shivamgupta62851@gmail.com>
2024-03-07 13:24:23 +0530

    https://github.com/Ransh0/Form

--- src/userdatabase/mongose-connection.js
+++ src/userdatabase/mongose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const users_collection1 = require('./userdata')
 
-mongoose.connect('mongodb://localhost:27017/userdataC')
+mongoose.connect('mongodb+srv://shivamgupta62851:usha89007@cluster1.jua8jqz.mongodb.net/urdata')
 .then(()=>{console.log('mongo connection succesfully')})
 .catch((err)=>{console.log(err)})
